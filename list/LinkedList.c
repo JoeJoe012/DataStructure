7d64552c14e934ea60e246b3ab543c15aedef43f
JoeJoe012 <>
2025-03-07 23:39:50 +0800

    update LinkedList.c

--- list/LinkedList.c
+++ list/LinkedList.c
@@ -11,30 +11,30 @@ struct ListNode {
 void init(Node head) {head->next = NULL;}
 /**
  * 将指定元素插入到此链表中的指定位置
- *   ·单链表中的结点都有一个指向后继结点的指针, 因此插入操作的关键是找到指定位置的前驱结点(也就是上一个结点)
- * @param head 操作链表的头结点
+ *   ·单链表中的节点都有一个指向后继节点的指针, 因此插入操作的关键是找到指定位置的前驱节点(也就是上一个节点)
+ * @param head 操作链表的头节点
  * @param element 待插入的元素
  * @param index 插入的位置, index的位序从1开始
  * @return 插入成功返回true, 否则返回false
  */
 bool insertList(Node head, E element, int index) {
     if (index < 1) return false;
-    while (--index) {    //while(--index){}条件表达式循环index-1次, 寻找目标位置的前驱结点
+    while (--index) {    //while(--index){}条件表达式循环index-1次, 寻找目标位置的前驱节点
         head = head->next;
-        if (head == NULL)    //若寻找过程中没有后续的结点, 则说明index超出可插入的范围
+        if (head == NULL)    //若寻找过程中没有后续的节点, 则说明index超出可插入的范围
             return false; 
     }
     Node newNode = malloc(sizeof(struct ListNode));
     if (newNode == NULL) return false;
     newNode->element = element;
-    newNode->next = head->next;    //到这步, head指向目标位置的前驱结点
+    newNode->next = head->next;    //到这步, head指向目标位置的前驱节点
     head->next = newNode;
     return true;
 }
 /**
  * 删除此链表中指定位置的元素
- *   ·单链表删除操作的关键是找到目标位置的前驱结点
- * @param head 操作链表的头结点
+ *   ·单链表删除操作的关键是找到目标位置的前驱节点
+ * @param head 操作链表的头节点
  * @param index 删除的位置, index的位序从1开始
  * @return 若删除成功返回true, 否则返回false
  */
@@ -45,7 +45,7 @@ bool deleteList(Node head, int index) {
         if (head == NULL)
             return false;
     }
-    if (head->next == NULL) {    //待删除结点为NULL则直接返回false
+    if (head->next == NULL) {    //待删除节点为NULL则直接返回false
         return false;
     }
     Node targetNode = head->next;
